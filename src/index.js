0e4f3d7212024f14b718e5642ec0b60160bfbf43
cptShell <repozheka@gmail.com>
2020-09-26 15:06:58 +0300

    fix: expressionCalculator works correctly with 'a/0' cases & different spaces between numbers & operations

--- src/index.js
+++ src/index.js
@@ -1,48 +1,68 @@
-/*function eval() {
+function eval() {
     // Do not use eval!!!
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    expressionCalculator
-}*/
-
-function calculate (arr) {
-    const [a,operation,b] = arr;
-    console.log(a, operation ,b);
-
-    switch (operation) {
-        case "*":
-            return +a * +b + "";
-        case "/":
-            return +a / +b + "";
-        case "+":
-            return +a + +b + "";
-        case "-":
-            return +a - +b + "";
-    }
-}
-
-function eval (exp) {
-    exp = exp.split(" ");
+function expressionCalculator(exp) {
+    
+    exp = exp.replace(/[*/+-]/g, " $& ").split(" ").reduce((res,v) => v != "" ? res.concat(v) : res , [])
     let operationIndex;
-
+    
     while (exp.includes("*") || exp.includes("/")) {
         operationIndex = exp.findIndex((v,i) => v == "*" || v == "/" ? i : false);
         exp.splice(operationIndex-1, 3, calculate([exp[operationIndex - 1], exp[operationIndex], exp[operationIndex + 1]]));
-        console.log(exp);
     }
+    
+    if(exp.includes("error")) throw new Error("TypeError: Division by zero.");
 
     while (exp.includes("+") || exp.includes("-")) {
         operationIndex = exp.findIndex((v,i) => v == "+" || v == "-" ? i : false);
         exp.splice(operationIndex-1, 3, calculate([exp[operationIndex - 1], exp[operationIndex], exp[operationIndex + 1]]));
-        console.log(exp);
+    }
+    console.log(+exp[0]);
+    return +exp[0];
+}
+
+module.exports = {
+    expressionCalculator
+}
+
+function calculate (arr) {
+    const [a,operation,b] = arr;
+
+    switch (operation) {
+        case "*":
+            return parseFloat(a) * parseFloat(b);
+        case "/":
+            if(b === "0") return "error";
+            return parseFloat(a) / parseFloat(b);
+        case "+":
+            return parseFloat(a) + parseFloat(b);
+        case "-":
+            return parseFloat(a) - parseFloat(b);
     }
 }
 
-const exp = "2 + 2 / 2 * 2"
-eval(exp)
+//function eval (exp) {
+//    exp = exp.split(" ");
+//    let operationIndex;
+//
+//    while (exp.includes("*") || exp.includes("/")) {
+//        operationIndex = exp.findIndex((v,i) => v == "*" || v == "/" ? i : false);
+//        exp.splice(operationIndex-1, 3, calculate([exp[operationIndex - 1], exp[operationIndex], exp[operationIndex + 1]]));
+//        console.log(exp);
+//    }
+//
+//    while (exp.includes("+") || exp.includes("-")) {
+//        operationIndex = exp.findIndex((v,i) => v == "+" || v == "-" ? i : false);
+//        exp.splice(operationIndex-1, 3, calculate([exp[operationIndex - 1], exp[operationIndex], exp[operationIndex + 1]]));
+//        console.log(exp);
+//    }
+//
+//    return exp;
+//}
+//
+//const exp = "2 + 2 / 2 * 2";
+//eval(exp)
+
+console.log("2+2 / 2*2".replace(/[*/+-]/g, " $& ").split(" ").reduce((res,v) => v != "" ? res.concat(v) : res , []))
