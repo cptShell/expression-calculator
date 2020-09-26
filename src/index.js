f3c50ecd59b636501e1d3b47fe0c4410fb6c70c9
cptShell <repozheka@gmail.com>
2020-09-26 14:13:51 +0300

    feat: add solution. While dont work without brackets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function eval() {
+/*function eval() {
     // Do not use eval!!!
     return;
 }
@@ -9,4 +9,40 @@ function expressionCalculator(expr) {
 
 module.exports = {
     expressionCalculator
+}*/
+
+function calculate (arr) {
+    const [a,operation,b] = arr;
+    console.log(a, operation ,b);
+
+    switch (operation) {
+        case "*":
+            return +a * +b + "";
+        case "/":
+            return +a / +b + "";
+        case "+":
+            return +a + +b + "";
+        case "-":
+            return +a - +b + "";
+    }
 }
+
+function eval (exp) {
+    exp = exp.split(" ");
+    let operationIndex;
+
+    while (exp.includes("*") || exp.includes("/")) {
+        operationIndex = exp.findIndex((v,i) => v == "*" || v == "/" ? i : false);
+        exp.splice(operationIndex-1, 3, calculate([exp[operationIndex - 1], exp[operationIndex], exp[operationIndex + 1]]));
+        console.log(exp);
+    }
+
+    while (exp.includes("+") || exp.includes("-")) {
+        operationIndex = exp.findIndex((v,i) => v == "+" || v == "-" ? i : false);
+        exp.splice(operationIndex-1, 3, calculate([exp[operationIndex - 1], exp[operationIndex], exp[operationIndex + 1]]));
+        console.log(exp);
+    }
+}
+
+const exp = "2 + 2 / 2 * 2"
+eval(exp)
